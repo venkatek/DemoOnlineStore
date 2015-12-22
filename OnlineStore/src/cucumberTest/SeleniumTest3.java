ba28a915a6d33026224da6e3684475b1c5c5f613
Venkatesh <Venkatesh@Venkatesh-PC>
2015-12-22 21:03:34 +0530

    Test commit3

--- OnlineStore/src/cucumberTest/SeleniumTest3.java
+++ OnlineStore/src/cucumberTest/SeleniumTest3.java
@@ -35,6 +35,7 @@ public class SeleniumTest3 {
 	        System.out.println("Launched and Navigated through Anthem Portal successfully");
 	        System.out.println("Launched and Navigated through Anthem Portal successfully");
 	        System.out.println("Launched and Navigated through Anthem Portal successfully");
+	        System.out.println("Launched and Navigated through Anthem Portal successfully");
 	        
 	        }
 }
